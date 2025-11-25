f1de8e984928a9e57a773df2ac0f89bfb1becdfd
shu12388y <spl.sp999@gmail.com>
2025-11-25 19:40:33 +0530

    added lesson 1

--- lesson-1/main.py
+++ lesson-1/main.py
@@ -4,6 +4,23 @@ from array import *
 
 def main()->None:
     arr = array('b',[1,2,4])
-    print(arr)
+    # print(arr)
 
 main()
+
+# Class 
+
+class Test:
+    x = 0       # class variable
+    def __init__(self):
+        self.a = 2          # instance variable
+        self. b = 3
+        
+    def fun():
+        print("function")
+
+
+t1 = Test
+print(t1.x)
+t1.fun()
+
