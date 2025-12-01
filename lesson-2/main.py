e93836c0944ebba4be3ca6656368ac2e40ba08e7
shu12388y <spl.sp999@gmail.com>
2025-12-01 21:06:10 +0530

    added sll:

--- lesson-2/main.py
+++ lesson-2/main.py
@@ -2,40 +2,41 @@
 class SLL:
     def __init__(self,head=None):
         self.head = head
-        
-        
+
+
 class Node:
     def __init__(self, item=None,next=None):
         self.item = item
         self.next = next
         
         
-n2 = Node(item=14,next=None)
-n1 = Node(item=12,next=n2)
-start = SLL(head=n1)
-
 '''
 [start | 200] -> [12 | 300] -> [14 | 400] -> [15 | None] 
       100           200           300           400
 '''
 
 
-listNode = list()
-listNode.append({
-    "node":"start",
-    "addr":start.head
-})
-listNode.append({
-    "node":"node",
-    "addr":n1.next,
-    "item":n1.item
-})
-listNode.append({
-    "node":"node",
-    "addr":n2.next,
-    "item":n2.item
-})
 
-for i in listNode:
-    print(i,end="\n")
 
+# n2 = Node(item=13,next=None)
+# n1 = Node(item=12,next=n2)
+# head = SLL(head=n1)
+
+
+
+N = int(input("Enter the number of node: "))
+i = 0
+ref_node = None 
+while i <= N:
+    item = int(input("Enter value: "))
+    if ref_node == None:
+        n = Node(item=item,next=None) 
+        ref_node = n
+        head = SLL(head=n)
+        print(ref_node,head,end="\n")
+        i = i +1
+    else:
+        n = Node(item=item,next=ref_node)
+        ref_node = n
+        print(ref_node,end="\n")
+        i =  i+1
