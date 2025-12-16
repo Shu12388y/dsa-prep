007836aaea05efcfa9dbf41633b68de443750ba6
shu12388y <spl.sp999@gmail.com>
2025-12-16 21:55:10 +0530

    added new esson

--- matrix/main.py
+++ matrix/main.py
@@ -2,15 +2,36 @@
 
 mat = [[1,2,3],[4,5,6],[7,8,9]]
 
-def printIt(arr):
+# def printIt(arr):
+#     for i in range(len(arr)):
+#         if i % 2 == 0:
+#             for j in range(len(arr)):
+#                 print(arr[i][j])
+#         else:
+#             for j in reversed(range(len(arr))):
+#                 print(arr[i][j])
+                
+                
+# printIt(mat)
+# 1,2,3,6,5,4,7,8,9
+
+
+
+def printBoundary(arr):
     for i in range(len(arr)):
-        if i % 2 == 0:
+        if i == 0:
             for j in range(len(arr)):
                 print(arr[i][j])
-        else:
-            for j in reversed(range(len(arr))):
+        
+        if i != len(arr)-1 and i != 0 and i != len(arr):
+            for j in range(len(arr)):
+                if j == 0 or j == len(arr)-1:
+                    print(arr[i][j])
+
+        if i == len(arr)-1:
+            for j in range(len(arr)):
                 print(arr[i][j])
                 
                 
-printIt(mat)
-# 1,2,3,6,5,4,7,8,9
+printBoundary(mat)
+# 1,2,3,4,6,7,8,9
